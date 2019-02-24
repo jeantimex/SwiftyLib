02147ee01912a8a54530c8d6e4a6c4accd92ede5
jeantimex <jean.timex@gmail.com>
2019-02-24 09:58:21 -0800

    Initial project setup

---
+++ SwiftyLib/SwiftyLib.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLib.h
+//  SwiftyLib
+//
+//  Created by Yong Su on 2/23/19.
+//  Copyright © 2019 Yong Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyLib.
+FOUNDATION_EXPORT double SwiftyLibVersionNumber;
+
+//! Project version string for SwiftyLib.
+FOUNDATION_EXPORT const unsigned char SwiftyLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLib/PublicHeader.h>
+
+
---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Yong Su on 2/23/19.
+//  Copyright © 2019 Yong Su. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
